6943f29d0645f1d4427a7fcbdbccfa486f3f5bba
Ashutosh Pandey <baralbisnupriya7@gmail.com>
2022-02-14 16:35:09 +0530

    whileloopprogram

---
+++ WhileLoop/power.sh
@@ -0,0 +1,12 @@
+
+
+read -p  " Enter the value of n :" n
+ i=1;
+ result=0;
+ while [ $i -le $n ] && [ $result -lt 256 ]
+ do
+      result=$((2*$i))
+      echo $result
+      i=$((i+1))
+ done
+	  
